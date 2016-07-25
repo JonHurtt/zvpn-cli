97d978ad0ed04084ebdb96fe401907e05034d111
Jonathan Hurtt <JonHurtt@gmail.com>
2016-07-25 16:55:50 -0400

    Refactor with Promises

--- app.js
+++ app.js
@@ -1,26 +1,32 @@
 var dateFormat = require('dateformat');
 var now = new Date();
 var pjson = require('./package.json');
-//var fs = require('fs');
 var fs = require('file-system');
 var csv = require('csv');
 var constants = require('./constants.json');
 var zen_regions = require('./zen_regions.json');
 var client_domain = constants.client_domain;	
+
 var newline = "\n";
-var spacer = "\n#++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n";
+var spacer = "#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++";
+
 var locations = [];
-var cli_directory = 'output/';
-var csv_directory = 'output/';
+var output_directory = 'output/';
+var cli_directory = output_directory;
+var csv_directory = output_directory;
+var debug_directory = output_directory;
+var debug = '';
 
-
+/*******************************************************/
+//YARGS
+/*******************************************************/
 var argv = require('yargs')
-	.command('build', 'CSV File for import', function (yargs) {
+	.command('bulk', 'CSV File for import', function (yargs) {
 		return yargs.options({ //Returning Options as third paramter of .command(cmd, desc, [module])
 			path: {
 				demand: true,
 				alias: 'p',
-				description: 'path for CSV',
+				description: 'path for CSV {loc,client_name,pass,zen}',
 				type: 'string'
 			}
 		}//end of options function
@@ -37,13 +43,43 @@ var command = argv._[0]; //underscore named argument
 var csv_filepath = argv.path; // --firstName named argument
 
 
-
+/*******************************************************/
+//Utilty functinons
+/*******************************************************/
 function isEmpty(str) {
   return typeof str == 'string' && !str.trim() || typeof str == 'undefined' || str === null;
 }
 
+/*******************************************************/
+//Create Directories needed to write files.
+/*******************************************************/
 
+function create_directories(){
+	return new Promise(function (resolve, reject){		
+		log_debug(spacer);
+		log_debug('start create_directories');
+		
+	
+		cli_directory += dateFormat(now, "yyyy-mm-dd")+"/";
+		csv_directory = cli_directory+"csv/";
+		debug_directory = cli_directory;
+		
+		fs.mkdir(cli_directory, function(err) {});
+		fs.mkdir(csv_directory, function(err) {});
+		
+		log_debug('end create_directories');
+		log_debug(spacer);
+		
+		resolve();
+		});
+}//end create_directories
+
+
+/*******************************************************/
+//Adding a single locaiton to the array locations
+/*******************************************************/
 function add_location(data){
+	
 	var location = {
 		name: data[0],
 		client_name: data[1],
@@ -53,62 +89,75 @@ function add_location(data){
 		f_gateway: ''
 	};//defining an empty object
 	
-	//console.log(data);
-	//console.log(location);
+	log_debug("===add_location(data)===");
+	log_debug('data.toString()');
+	log_debug(data.toString());
+	
 	//Define ZEN Node
 	gateways = determine_zen(location.zen_code);
 	
 	location.gateway = gateways.primary;
 	location.f_gateway = gateways.failover;	
-	
-		
-	locations.push(location);		
-	//console.log(locations);
+			
+	locations.push(location);
 }//end add_location
 	
+/*******************************************************/
+//Utilty take parsed data and create locations
+/*******************************************************/
+function add_locations(data){
+	return new Promise(function (resolve, reject){		
+
+		log_debug('===add_locations(locations)===');
 	
-function add_locations(locations){
-		locations.forEach(function (location){
-
-		console.log('Adding Location ' + location[0] + ' ');
-		add_location(location)
-	})
-	
-	/*
-		console.log(spacer);	
-		console.log('Locations Added');
-		console.log(locations);	
-		console.log(spacer);		
-	*/
-}//end add_locations
+		data.forEach(function (location){
+			log_debug(spacer);
+			log_debug('***Adding Location ' + location[0] + '***');
+			add_location(location)
+			log_debug(spacer);
+		})
+		
+		
+		resolve();
+		
+	})//end promise
+}//end of locations
 
 
-
-
+/*******************************************************/
+//Determine the Gateways based on ZEN Code
+/*******************************************************/
 function determine_zen(zen_code){
+	log_debug('===determine_zen(zen_code)===');
+	log_debug('zen_code: '+zen_code);
+
 	var gateways = {"primary": "", "failover":""};
 
-	console.log("Zen Code:"+zen_code);
-	console.log(zen_regions[zen_code].zen_ip);
-	
-
 	failover_zen = zen_regions[zen_code].failover;
-	console.log("Failover Zen Code:"+failover_zen);
-	console.log(zen_regions[failover_zen].zen_ip);
 	
 	gateways.primary = zen_regions[zen_code].zen_ip
 	gateways.failover = zen_regions[failover_zen].zen_ip;
+	
+	log_debug('Zen IP: ' + zen_regions[zen_code].zen_ip);
+	log_debug('Failover zen_code: '+failover_zen);
+
+	log_debug('Failover Zen IP: ' + zen_regions[failover_zen].zen_ip);
+
+	
 	
 	return gateways;
 }
 
 
-function getCLI(location){
-	var cli;
-	//console.log("starting getCLI");
-	//console.log("Variables {client_name: " + location.client_name + ", password: " + location.password + ", gateway: " + location.gateway + ", f_gateway: "+location.f_gateway+"}");
+/*******************************************************/
+//Generate the CLI Commands based on Location
+/*******************************************************/
+function get_commands(location){
+	log_debug('get_commands(location)');
+	log_debug("{c_n" + location.client_name + ", p: " + location.password + ", g: " + location.gateway + ", f_g: "+location.f_gateway+"}");
 	
-	cli = spacer;	
+	var cli;
+		cli = spacer;	
 	cli += "#Start of Configuration for "+ location.name ;
 	cli += spacer;
 	cli += "user-profile Public-VPN-User security deny ipv6";
@@ -117,32 +166,35 @@ function getCLI(location){
 	cli += newline;
 	cli += "vpn l2l-access-list VPN-ACL src-ip 10.0.0.0/16 dst-ip 0.0.0.0/0";
 	cli += newline;	
-	cli += getVPN(location.gateway, location.client_name, location.password, false);
+	cli += get_vpn_commands(location.gateway, location.client_name, location.password, false);
 	cli += newline;	
 	
 	if(!isEmpty(location.f_gateway)){
-		//console.log('Location is configured with Failover Gateway');
-		cli += getVPN(location.f_gateway, location.client_name, location.password, true);	
+		log_debug('Location is configured with Failover Gateway');
+		cli += get_vpn_commands(location.f_gateway, location.client_name, location.password, true);	
 	}else{		
 		//console.log('Location is NOT configured with Failover Gateway');
 	}
 	cli += spacer;
 	cli += "#End of Configuration for "+ location.name ;
 	cli += spacer;
-	
+	log_debug(spacer)
+
 	return cli;
 	
-}//end getCLI()
-function getVPN(gateway, client_name, password, failover){
+}//end get_commands()
+
+
+/*******************************************************/
+//Generate VPN/Gateway specfic caommands
+/*******************************************************/
+function get_vpn_commands(gateway, client_name, password, failover){
 	var cli_vpn;
+	var vpn ='';
 	
-	if(failover === true ){
-		var VPN = "VPN-2"
-	}else{
-		var VPN = "VPN-1"
-	}
+	if(failover === true ){VPN = "VPN-2"}else{var VPN = "VPN-1"}
 	
-	//console.log("starting getVPN("+gateway+", "+client_name+", "+password+", "+failover+")");
+	log_debug("starting get_vpn_commands("+gateway+", "+client_name+", "+password+", "+failover+")");
 	cli_vpn = "vpn client-ipsec-tunnel "+VPN+" vpn-mode layer-3 lan-to-lan-vpn";
 	cli_vpn += newline;	
 	cli_vpn += "vpn ipsec-tunnel "+VPN+" gateway "+gateway+" client-name "+client_name+"@"+client_domain+" password "+password+"";
@@ -176,16 +228,20 @@ function getVPN(gateway, client_name, password, failover){
 		cli_vpn += "vpn tunnel-policy ZVPN client ipsec-tunnel "+VPN+" primary";
 	}
 	return cli_vpn;
-}//end getVPN()
+}//end get_vpn_commands()
 
+
+/*******************************************************/
+//Generate the File for CLI Commands
+/*******************************************************/
 function generate_file(location){
 	return new Promise(function (resolve, reject){		
 		var output = "";
 		var filename = dateFormat(now, "yyyymmdd")+"-scli-"+location.client_name + ".txt"
 		var filepath = cli_directory + filename;
 		
-		//console.log(spacer);
-		//console.log("Generating CLI...");
+		log_debug(spacer); 
+		log_debug("Generating CLI for location "+location.name+"...");
 		output = spacer;	
 		output += "#Generated by " +	pjson.name + " v"+pjson.version;
 		output += newline;
@@ -201,57 +257,88 @@ function generate_file(location){
 		output += newline;
 		output += "#debug: "+location.zen_code+"|"+location.gateway+"|"+location.f_gateway;		
 		output += spacer;
-		output +=getCLI(location);
+		output +=get_commands(location);
 		resolve({output: output, filepath: filepath});	
 		}//end function
 	);//end return
 }//end generate_file()
 
+/*******************************************************/
+//Write a String to File
+/*******************************************************/
 function write_to_file(output, filepath){
-	console.log("++Generating "+filepath+" ........");
-
-	
+	log_debug("++Generating "+filepath+" ........");
 	fs.writeFile(filepath, output, function(err) {
 	    if(err) {
 	        return console.log(err);
 	    }
-	    console.log("+++Succesful Generation of "+filepath+"!");
+	    log_debug("+++Succesful Generation of "+filepath+"!");
 	});	
 }//end write_to_file()
 
-function bulk_generate(locations){
-	console.log('+Bulk Generation has begun...');
+/*******************************************************/
+//Bulk Generate
+/*******************************************************/
+function bulk_generate_file(locations){
+	return new Promise(function (resolve, reject){		
+	log_debug('+Bulk Generation has begun...');
 	locations.forEach(function (location){generate_file(location).then(function(data){write_to_file(data.output, data.filepath);})})
-}//end bulk_generate
-
-function parse_csv(csv_file){
-	/*CSV Format: name,client_name,gateway,fgateway*/	
-	var parser = csv.parse({delimiter: ',', comment: '#'}, function(err, data){
-		console.log("Parsed Data");
-		console.log(data);
-		add_locations(data);
-		create_directories();
-		bulk_generate(locations);
-		generate_location_csv(locations);
-		generate_vpn_cred_csv(locations);
-
+	resolve();
 	});
-		
-	fs.createReadStream(csv_file).pipe(parser);
-}//end parse_csv
+}//end bulk_generate_file
 
-function create_directories(){
-	/*FIX THE CREATE DIRECTORIES*/		
-		cli_directory += dateFormat(now, "yyyy-mm-dd")+"/";
-		csv_directory = cli_directory+"csv/";
-		
-		fs.mkdir(cli_directory, function(err) {});
-		fs.mkdir(csv_directory, function(err) {});
-	
-}
 
+
+/*******************************************************/
+//Parse and Process the CSV File and generate csv and cli files
+/*******************************************************/
+function process_csv(csv_file){
+	return new Promise(function(resolve,reject){
+		/*CSV Format: location,client_name,password,zen_code*/	
+		var parser = csv.parse({delimiter: ',', comment: '#'}, function(err, data){
+			
+			log_debug('===csv input===');
+			log_debug(data.toString())
+			log_debug('===add_locations(data)===');
+			add_locations(data).then(function(){
+				log_debug(spacer);												
+				log_debug('===create_directories()===');
+				log_debug(spacer);												
+				create_directories().then(function(){	
+					log_debug(spacer);								
+					log_debug('===bulk_generate_file(locations)===');
+					log_debug(spacer);								
+					bulk_generate_file(locations).then(function(){
+						return new Promise(function(resolve, reject){
+							log_debug(spacer);
+							log_debug('===generate_location_csv(locations)===');
+							log_debug(spacer);							;
+							generate_location_csv(locations);
+							log_debug(spacer);							
+							log_debug('===generate_vpn_cred_csv(locations)===');
+							log_debug(spacer);							
+							generate_vpn_cred_csv(locations);
+							resolve();
+						})//end Promise
+					}).then(function(){			
+					generate_debug();
+					})//end then generate_debug
+				});//end then create_directories
+			})//end then add_locations	
+		});//end parser object
+		
+		log_debug('===createReadStream(csv_file)===');
+		fs.createReadStream(csv_file).pipe(parser);
+		resolve();
+	});
+}//end process_csv
+
+
+/*******************************************************/
+//Generate Location CSV File
+/*******************************************************/
 function generate_location_csv(locations){
-	console.log('+Location CSV generation has begun...');
+	log_debug('+Location CSV generation has begun...');
 	//function to generate locations csv
 	var output = '';
 	var location_csv_filename = dateFormat(now, "yyyymmdd")+"-vpn-location.csv";
@@ -261,26 +348,21 @@ function generate_location_csv(locations){
 	locations.forEach(function (location){
 		output += "+,VPN,"+location.name+",FQDN,"+location.client_name+"@"+client_domain+"\n";
 	})
-	
-	console.log("++Generating "+location_csv_filepath+" ........");
-	
-	fs.writeFile(location_csv_filepath, output, function(err) {
-	    if(err) {
-	        return console.log(err);
-	    }
-	    console.log("+++Succesful Generation of "+location_csv_filepath+"!");
-	});	
 
-}
+	write_to_file(output,location_csv_filepath);
+}//end generate_location_csv
 
+
+/*******************************************************/
+//Generate VPN Credentials CSV File
+/*******************************************************/
 function generate_vpn_cred_csv(locations){
-	console.log('+Location VPN Credentials generation has begun...');
+	log_debug('+Location VPN Credentials generation has begun...');
 	/*
 	Action,PSK Type,VPN User Name,Comments,Pre-Shared Key,THIS IS A HEADER LINE WHICH WILL NOT BE IMPORTED - YOUR DATA MUST START FROM LINE 2
 	+,UFQDN,{client_name}@domain.com,Location:{Location}[{Password}],{Password},
 	*/
 	
-	//console.log(locations);
 	//function to generate locations csv
 	var output = 'Action,PSK Type,VPN User Name,Comments,Pre-Shared Key,THIS IS A HEADER LINE WHICH WILL NOT BE IMPORTED - YOUR DATA MUST START FROM LINE 2\n';
 	var vpn_csv_filename = dateFormat(now, "yyyymmdd")+"-vpn-credentials.csv";
@@ -288,48 +370,60 @@ function generate_vpn_cred_csv(locations){
 	
 	//+,VPN,{Location},FQDN,{client_name}@domain.com
 	locations.forEach(function (location){
-		output += "+,UFQDN,"+location.name+"@"+client_domain+",Location:"+location.client_name+"["+location.password+"],"+location.password+",\n";
+		output += "+,UFQDN,"+location.client_name+"@"+client_domain+",Location:"+location.client_name+"["+location.password+"],"+location.password+",\n";
 	})
 	
-	console.log("++Generating "+location_csv_filepath+" ........");
-	
-	fs.writeFile(location_csv_filepath, output, function(err) {
-	    if(err) {
-	        return console.log(err);
-	    }
-	    console.log("+++Succesful Generation of "+location_csv_filepath+"!");
-	});	
+	write_to_file(output,location_csv_filepath);
+}//end generate_vpn_cred_csv
 
+/*******************************************************/
+//Generate Summary 
+/*******************************************************/
+function log_debug(input){
+	//console.log(input);
+	debug += "[" + dateFormat(now, "ddmmyyyy:HH:MM:ss") + "]:" + input + "\n";
 }
-	
-
-//console.log(getCLI("[GATEWAY]", "[CLIENT_NAME]", "[PASSWORD]","[FAILOVER_GATEWAY]" ));
-//console.log(getCLI("[GATEWAY]", "[CLIENT_NAME]", "[PASSWORD]" ));
-//generate_file("[GATEWAY]", "zvpn-br-scli", "[PASSWORD]","[FAILOVER_GATEWAY]" )
-//generate_file(locations[0])
-/*for (i=0; i < 5; ++i){
-	console.log('Add Location' + i + ' to locations');
-	add_location('location'+i, 'password'+i, 'gateway'+i, 'f_gateway'+i)
-}*/
 
 
-/*********************************************
-	Start of Application
-**********************************************/
+/*******************************************************/
+//Generate Summary 
+/*******************************************************/
+function generate_debug(){
+	var debug_filename = dateFormat(now, "yyyymmdd")+"-debug.txt";
+	var debug_filepath = debug_directory + debug_filename;
+
+	//console.log("++++++++++++++++++++START OF DEBUG OUTPUT++++++++++++++++++++");
+	//console.log(debug);
+	//console.log("++++++++++++++++++++END OF DEBUG OUTPUT++++++++++++++++++++");
+
+	log_debug("++Generating "+debug_filepath+" ........");
+	write_to_file(debug,debug_filepath);
+}//end generate_debug
+
+
+/*******************************************************/
+//	Start of Application
+/*******************************************************/
 
 console.log(spacer);
 console.log(pjson.name + "- v"+pjson.version);
 console.log(pjson.homepage);
 console.log(spacer);
 
-if(command === 'build'){
-	parse_csv(csv_filepath);
+if(command === 'bulk'){
+	process_csv(csv_filepath);
 }else{
-	//console.log(zen_regions);
-	//determine_zen('ATL');
+
+/*
+1 - Select Single or Bulk
+--Single 
+--ask for 
+--Bulk - Ask for File
+---Parse File and output
+*/
 
 }
-console.log(spacer);
+
 
 
 
